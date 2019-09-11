8e18b006c8d68069a6e9ffff839e8a2fb04c0322
Eyal Cohen <ceyal74@gmail.com>
2019-09-11 12:16:06 +0300

    Added test and pages
    Added test and pages

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d2b54aa-0318-4224-90ba-797a8b50d998")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pages.PagesPO;
+using Automation.Core.Infrastructure;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestExample()
+        {
+            WebRunner webRunner;
+            webRunner = new WebRunner(TestProfile.IEProfile);
+            LoginPage loginPage = new LoginPage(webRunner);
+            loginPage.UserName.Text = "User1";
+            loginPage.Pass.Text = "MyPassword";
+            loginPage.LoginBtn.Click();
+            //Assert.IsTrue("TBD");
+            webRunner.Close();
+
+        }
+    }
+}
